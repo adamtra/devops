f1685c4d0ea0de66c8e5d926ac7c82a8ea17cf09
Adam Trawiński <adam.trawinski@poczta.fm>
2020-04-16 09:02:04 +0200

    Node + redis + pg

---
+++ mydemoapp/backend/index.js
@@ -0,0 +1,95 @@
+const keys = require('./keys');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const redis = require('redis');
+
+const app = express();
+app.use(bodyParser.json());
+
+const client = redis.createClient({
+    host: keys.redisHost,
+    port: keys.redisPort
+});
+
+const { Pool } = require('pg');
+
+const pgClient = new Pool({
+    user: keys.pgUser,
+    host: keys.pgHost,
+    database: keys.pgDatabase,
+    password: keys.pgPassword,
+    port: keys.pgPort
+});
+
+pgClient.on('error', () => {
+    console.log('Error');
+});
+
+pgClient.query('CREATE TABLE IF NOT EXISTS results (number INT)')
+    .catch((err) => {
+        console.log(err);
+    });
+
+const nwd = (input1, input2) => {
+  let tmp;
+  while (input2) {
+    tmp = input1 % input2;
+    input1 = input2;
+    input2 = tmp;
+  }
+  return input1;
+};
+
+const addResult = (value) => {
+    return new Promise((resolve, reject) => {
+        pgClient
+          .query("INSERT INTO results (number) VALUES ($1)", [value])
+          .then(() => {
+            resolve();
+          })
+          .catch((err) => {
+            console.log(err);
+            reject();
+          });
+    })
+}
+
+app.get("/", (req, res) => {
+    pgClient.query('SELECT DISTINCT number FROM results')
+        .then((data) => {
+            return res.json(data.rows);
+        }).catch((err) => {
+            console.log(err);
+            return res.status(500);
+        });
+});
+
+app.get("/:n1/:n2", (req, res) => {
+  const n1 = parseInt(req.params.n1);
+  const n2 = parseInt(req.params.n2);
+  let input1, input2;
+  if (n1 > n2) {
+    input1 = n1;
+    input2 = n2;
+  } else {
+    input1 = n2;
+    input2 = n1;
+  }
+  const key = `${input1},${input2}`;
+  client.get(key, async (err, value) => {
+    if (value !== null) {
+        await addResult(value);
+        return res.send(`${value}`);
+    }
+    const result = nwd(input1, input2);
+    client.set(key, result);
+    await addResult(result);
+    return res.send(`${result}`);
+  });
+});
+
+app.listen(3000, () => {
+    console.log('Server listenig on port 3000');
+});
