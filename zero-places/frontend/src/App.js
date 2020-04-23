b3079b18ca11682de6a4081422e713628792008c
Adam Trawiński <adam.trawinski@poczta.fm>
2020-04-23 09:59:02 +0200

    Wysłanie i odebranie danych z api

--- zero-places/frontend/src/App.js
+++ zero-places/frontend/src/App.js
@@ -1,26 +1,127 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  Button,
+  Card,
+  CardActions,
+  TextField,
+  InputAdornment,
+} from "@material-ui/core";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      a: 0,
+      b: 0,
+      c: 0,
+      showForm: true,
+      places: [null, null],
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  async handleSubmit(event) {
+    event.preventDefault();
+    const result = await axios.post('/api/results', {
+      a: this.state.a,
+      b: this.state.b,
+      c: this.state.c,
+    });
+    this.setState({
+      places: result.data,
+    });
+    this.setState({
+      showForm: false,
+    });
+  }
+
+  render() {
+    let container;
+    if (this.state.showForm) {
+      container = (
+        <div>
+          <form
+            onSubmit={this.handleSubmit}
+            id="function-form"
+            className="d-flex p-2"
+            autoComplete="off"
+          >
+            <TextField
+              className="px-2"
+              id="a"
+              name="a"
+              value={this.state.a}
+              onChange={this.handleInputChange}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">a=</InputAdornment>
+                ),
+                endAdornment: (
+                  <InputAdornment position="start">
+                    x<sup>2</sup>
+                  </InputAdornment>
+                ),
+              }}
+              required
+            />
+            <TextField
+              className="px-2"
+              id="b"
+              name="b"
+              value={this.state.b}
+              onChange={this.handleInputChange}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">b=</InputAdornment>
+                ),
+                endAdornment: (
+                  <InputAdornment position="start">x</InputAdornment>
+                ),
+              }}
+              required
+            />
+            <TextField
+              className="px-2"
+              id="c"
+              name="c"
+              value={this.state.c}
+              onChange={this.handleInputChange}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">c=</InputAdornment>
+                ),
+              }}
+              required
+            />
+          </form>
+          <CardActions>
+            <Button
+              form="function-form"
+              variant="contained"
+              type="submit"
+              color="primary"
+            >
+              Oblicz miejsca zerowe
+            </Button>
+          </CardActions>
+        </div>
+      );
+    } else {
+      container = <h2>Wysłano formularz: {this.state.places}</h2>;
+    }
+    return <Card className="p-2 m-2">{container}</Card>;
+  }
 }
-
-export default App;
