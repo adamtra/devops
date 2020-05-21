02c9d59b9f86678adc6a77c43ea11173c3f0ee67
Adam Trawiński <adam.trawinski@poczta.fm>
2020-05-21 09:16:49 +0200

    Config map i  podpięcie bazy danych

--- cluster/backend/index.js
+++ cluster/backend/index.js
@@ -1,3 +1,4 @@
+const keys = require('./keys');
 const { v4: uuidv4 } = require("uuid");
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -8,27 +9,93 @@ const app = express();
 app.use(bodyParser.json());
 
 const redisClient = redis.createClient({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
+  host: keys.redisHost,
+  port: keys.redisPort,
   retry_strategy: () => 1000
 });
+
+const { Pool } = require('pg');
+
+const pgClient = new Pool({
+  user: keys.pgUser,
+  host: keys.pgHost,
+  database: keys.pgDatabase,
+  password: keys.pgPassword,
+  port: keys.pgPort
+});
+
+pgClient.on('error', () => {
+  console.log('Error');
+});
+
+const connect = () => {
+  pgClient.connect().then(() => {
+    pgClient
+      .query("CREATE TABLE IF NOT EXISTS requests (a varchar(255))")
+      .catch((err) => {
+        console.log(err);
+      });
+  }).catch(() => {
+    setTimeout(() => {
+      connect();
+    }, 1000);
+  });
+}
+
+connect();
+
+
+const addResult = (a) => {
+  return new Promise((resolve, reject) => {
+    pgClient
+      .query(
+        "INSERT INTO requests (a) VALUES ($1)",
+        [a]
+      )
+      .then(() => {
+        resolve();
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  })
+}
+
 
 const appId = uuidv4();
 const appPort = 5000;
 
-const appName = `AppId: ${appId}`;
-app.get('/:key', (req, res) => {
-  const { key } = req.params;
-  redisClient.get(key, (err, value) => {
+const appName = `${keys.welcomeMessage} AppId: ${appId}`;
+
+app.get('/', (req, res) => {
+  return res.send(appName);
+});
+
+app.get('/results', (req, res) => {
+  pgClient
+    .query("SELECT * FROM requests")
+    .then((data) => {
+      return res.json(data.rows);
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(500);
+    });
+});
+
+app.post('/', (req, res) => {
+  const { key } = req.body;
+  redisClient.get(key, async (err, value) => {
     if (err) {
       console.log(err);
-      return res.send(`${appName} Wystąpił błąd`);
+      return res.send(`${appId} Wystąpił błąd`);
     }
+    await addResult(key);
     if (value !== null) {
-      return res.send(`${appName} Znaleziono w cache`);
+      return res.send(`${appId} Znaleziono w cache`);
     }
     redisClient.set(key, "");
-    return res.send(`${appName} Dodano do cache`);
+    return res.send(`${appId} Dodano do cache`);
   });
 });
 
